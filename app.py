727aab95517626af2fd92a06b62baa66c2a0867f
justSid26 <lamasidharth25@gmail.com>
2025-04-03 00:45:51 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+about = 'This is a simple portfolio builder in which it will create a portfolio for you based on your inputs. The portfolio is also downloadable as a pdf. Try it, because it is free and convenient.'
+
+
+@app.route("/")
+def index():
+    return render_template("index.html", about=about)
+
+@app.route("/portfolio")
+def portfolio_maker():
+    return render_template("portfolio.html")
+
+@app.route("/login")
+def login_page():
+    return render_template("login.html")
+
+if __name__ == '__main__':
+    app.run( debug=True)
